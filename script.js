c1c0cbdae710c1f9879064001dbfa2031fbd98f1
Grayson Connor <grayson.bolon@yahoo.com>
2022-09-05 16:07:44 -0500

    order summary complete

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const togglePromo = document.querySelector(".toggle-promo");
+const promoCode = document.querySelector(".promo-code");
 
+togglePromo.addEventListener("change", () => {
+    togglePromo.checked ? promoCode.style.display = "block" : promoCode.style.display = "none";
+});
 
 
