10b851901d4a7d35ec155d854a2093d68eec87f0
Grayson Connor <grayson.bolon@yahoo.com>
2022-09-06 22:24:14 -0500

    done

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const togglePromo = document.querySelector(".toggle-promo");
 const promoCode = document.querySelector(".promo-code");
+const mobileMenu = document.querySelector(".toggle-menu-btn");
+const navigation = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", () => {
+    navigation.classList.toggle("open-menu");
+})
 
 togglePromo.addEventListener("change", () => {
     togglePromo.checked ? promoCode.style.display = "block" : promoCode.style.display = "none";
 });
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if (!isDropdownButton && e.target.closest("[data-dropdown") != null) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest("[data-dropdown");
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    })
+})
 
 
